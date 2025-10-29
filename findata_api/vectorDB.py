80275be5270e0736c0567f1c1e9163c2eaa03fdf
ae-tin <xodls4179@gmail.com>
2025-10-29 15:02:44 +0900

    feat: save data to vectorDB 함수 작성

--- findata_api/vectorDB.py
+++ findata_api/vectorDB.py
@@ -36,3 +36,46 @@ def get_qdrant_local(
         )
     # 존재하면 아무 것도 하지 않고 그대로 반환
     return client
+
+
+def save_vectorDB(
+    chunked_docs: List[str],
+    collection_name: str = "finance_products",
+    category: str = "deposit",
+    vector_size: int = 1024,
+    path: str = "./qdrant_localdb",
+) -> QdrantClient:
+    """
+    VectorDB에 Chunked data 저장하는 함수
+    - "BAAI/bge-m3" Embedding Model 사용
+    - Qdrant VectorDB 사용
+
+    arguments:
+        (List[str]) chunked_docs: Chunking된 금융데이터 리스트
+        (str) collection_name: 금융데이터 DB 이름
+        (str) category: 세부 카테고리
+        (int) vector_size: embedding vector size
+        (str) path: VectorDB 저장 경로
+    return:
+        QdrantClient: Qdrant VectorDB Client
+    """
+
+    # bge-m3 임베딩 모델 로드
+    model = BGEM3FlagModel("BAAI/bge-m3", use_fp16=False)
+    collection_name = collection_name + "_" + category
+    # Qdrant 초기화
+    client = get_qdrant_local(
+        collection_name=collection_name,
+        vector_size=1024,
+    )
+
+    points = []
+    for i, doc in enumerate(tqdm(chunked_docs, desc="임베딩 + 업로드 중")):
+        vec = model.encode([doc.page_content], return_dense=True)["dense_vecs"][0]
+        point_id = str(uuid.uuid4())
+        payload = {**doc.metadata, "chunk_id": i, "text": doc.page_content}
+        points.append(PointStruct(id=point_id, vector=vec, payload=payload))
+
+    client.upsert(collection_name=collection_name, points=points)
+    print(f"\n 업로드 완료: 총 {len(points)}개 chunk (Document 기반)")
+    return client
