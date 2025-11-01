2131bcf04e88899df75d52e68d46b36842b19383
yuna <yunao1@naver.com>
2025-11-01 20:24:09 +0900

    db: 금융 상품 데이터 MySQL로 받기

--- finbot/settings.py
+++ finbot/settings.py
@@ -82,9 +82,16 @@ WSGI_APPLICATION = "finbot.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'agent_ai',  # 데이터베이스 이름
+        'USER': 'root',      # 데이터베이스 사용자 이름
+        'PASSWORD': 'password3311!',  # 데이터베이스 사용자 비밀번호
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
