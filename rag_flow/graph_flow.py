4c2cf4aa985355e51cf3878fbfefaebc7b70fb62
ae-tin <xodls4179@gmail.com>
2025-11-03 17:22:49 +0900

    docs: docstring 양식 맞춰 작성

--- rag_flow/graph_flow.py
+++ rag_flow/graph_flow.py
@@ -28,6 +28,14 @@ class ChatSession:
         """
 
     def ask(self, query: str):
+        """
+        사용자의 질문을 받고 Langgraph를 거쳐 답변을 생성
+
+        Args:
+            query (str): 사용자의 질문 query 
+        Returns:
+            answer (str): Langgraph state의 answer
+        """
         self.state["query"] = query
         self.state = app_graph.invoke(self.state)
         return self.state["answer"]
@@ -35,7 +43,13 @@ class ChatSession:
     
 def keep_last_n(existing: List[Dict], new: List[Dict], n: int = 10) -> List[Dict]:
     """
-    최근 n개 항목만 유지하는 리듀서
+    최근 n개 항목만 유지하는 리듀서. State의 history List에 새로운 값을 추가하고 n개의 항목만 반환(유지)
+
+    Args:
+        existing (List[Dict]): 기존의 history List
+        new (List[Dict]): 새로운 history List
+    Returns:
+        List[Dict][-n:]: 새로운 history List를 포함하여 n개의 항목 유지
     """
     combined = (existing or []) + (new or [])
     return combined[-n:]  # 마지막 n개만 반환
@@ -45,14 +59,14 @@ keep_last_10 = partial(keep_last_n, n=10)
 
 class ChatState(TypedDict):
     """
-    graph를 구성할 State
+    graph를 구성할 State Class
     """
     
-    mode: str # chat mode : first conversation & first meet, first conversation & Nth meet, Nth conversation
+    mode: str # chat mode : (First_hello)first conversation & first meet, (Nth_hello)first conversation & Nth meet, (Normal_chat)Nth conversation
     search_method: str # search method : DB search, RAG search
-    query: str
-    history: Annotated[List[Dict[str, str]], keep_last_10]  # user & assistant message 쌍
-    answer: str
+    query: str # user query
+    history: Annotated[List[Dict[str, str]], keep_last_10]  # user, assistant message 쌍
+    answer: str # LLM answer
 
 
 # 노드 정의
@@ -60,6 +74,11 @@ class ChatState(TypedDict):
 def conditional_about_history(state: ChatState) -> Dict:
     """
     history에 따라 분기 발생 
+
+    Args:
+        state (TypedDict): Graph의 state
+    Returns:
+        Dict: state에 업데이트 할 mode dict, mode = ("First_hello", "Nth_hello", "Normal_chat")
     """
     # 복합 조건 평가
     if not state["history"]:
@@ -81,6 +100,11 @@ def conditional_about_history(state: ChatState) -> Dict:
 def mode_router(state: ChatState) -> Literal["First_hello", "Nth_hello", "Normal_chat"]:
     """
     Mode에 따라 라우팅
+
+    Args:
+        state (TypedDict): Graph의 state
+    Returns:
+        Literal: ["First_hello", "Nth_hello", "Normal_chat"] 중 하나의 값으로 제한
     """
     return state["mode"]
 
@@ -88,6 +112,11 @@ def mode_router(state: ChatState) -> Literal["First_hello", "Nth_hello", "Normal
 def first_conversation(state: ChatState) -> ChatState:
     """
     이전 history 아예 없이 첫 방문 첫 인사
+
+    Args:
+        state (TypedDict): Graph의 state
+    Returns:
+        Dict: state에 업데이트 할 history dict. 
     """
     
     hello = "안녕하세요. 첫 방문이시군요! 무엇을 도와드릴까요?"
@@ -98,12 +127,17 @@ def first_conversation(state: ChatState) -> ChatState:
 def nth_conversation(state: ChatState) -> ChatState:
     """
     이전 history 존재. 첫 인사.
-    history 요약해서 
+    history 요약해서 제공
+
+    Args:
+        state (TypedDict): Graph의 state
+    Returns:
+        Dict: state에 업데이트 할 history dict. 
     """
     histories = state["history"]
     questions = [ history["content"] for history in histories if history["role"] == "user"]
     
-    messages = [{"role": "system", "content": "너는 몇 개의 문장을 3단어로 요약해야해."}]
+    messages = [{"role": "system", "content": "너는 주어지는 몇 개의 문장을 3단어로 요약해야해."}]
     messages.append({"role": "user", "content": questions})  # 이전 질문들 모두 
 
     completion = client.chat.completions.create(model="gpt-4o-mini", messages=messages)
@@ -117,58 +151,105 @@ def nth_conversation(state: ChatState) -> ChatState:
 
 def conditional_about_query(state: ChatState) -> Dict:
     """
-    query에 따라 분기 발생 
+    query에 따라 분기 발생.(하려했으나 DB search는 다른 함수로 빠졌기 때문에 RAG search만 존재)
+
+    Args:
+        state (TypedDict): Graph의 state
+    Returns:
+        Dict: state에 업데이트 할 method dict, method = ("RAG_search") # DB_search
     """
     # 복합 조건 평가
-    query = state["query"]
-    if "검색" in query:
-        method = "DB_search"
-    else:
-        method = "RAG_search"
+    # query = state["query"]
+    # if "검색" in query:
+    #     method = "DB_search"
+    # else:
+    method = "RAG_search"
     return {
         "search_method": method,
     }
 
-def method_router(state: ChatState) -> Literal["DB_search", "RAG_search"]:
+def method_router(state: ChatState) -> Literal["RAG_search"]: #"DB_search", 
     """
     Search Method에 따라 라우팅
+
+    Args:
+        state (TypedDict): Graph의 state
+    Returns:
+        Literal: ["RAG_search"] 중 하나의 값으로 제한
     """
     return state["search_method"]
 
 def add_to_history(state: ChatState) -> ChatState:
     """
     이전 대화 기록을 유지하면서 새 user message 추가
+
+    Args:
+        state (TypedDict): Graph의 state
+    Returns:
+        history Dict: state의 history update
     """
-    history = state.get("history", [])
-    history.append({"role": "user", "content": state["query"], "state": "new"})
-    return {"history": history}
+    new_history = {"role": "user", "content": state["query"], "state": "new"}
+    return {"history": new_history}
 
 
 def DB_search(state: ChatState) -> ChatState:
     """
     대화 히스토리를 프롬프트에 포함해 답변 생성
-    """
-    messages = [{"role": "system", "content": ""}]
 
-    completion = client.chat.completions.create(model="gpt-4o-mini", messages=messages)
+    Args:
+        state (TypedDict): Graph의 state
+    Returns:
+        Dict: LLM의 답변과 새로운 history를 반환
+    """
+    DB_answer = "DB 검색 결과"
+    user_query = state["query"]
+    messages = [
+        {"role": "system", "content": "너는 금융 도메인 전문가이자 고객 상담 AI야. DB에서 제공된 정보를 근거로만 답변해야 해."},
+        {"role": "user", "content": f"다음은 DB에서 찾은 정보야:\n{DB_answer}"},
+        {"role": "user", "content": f"질문: {user_query}\n이 '정보'만 참고해서 사용자의 질문에 정확히 답변해줘."}
+    ]
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages,
+        max_tokens=400,
+    )
 
     answer = completion.choices[0].message.content
     # 히스토리에 assistant 응답 추가
-    new_history = state["history"] + [{"role": "assistant", "content": answer, "state": "new"}]
-    return state | {"answer": answer, "history": new_history}
+    new_history = {"role": "assistant", "content": answer, "state": "new"}
+    return {"answer": answer, "history": new_history}
 
 def RAG_search(state: ChatState) -> ChatState:
     """
-    대화 히스토리를 프롬프트에 포함해 답변 생성
+    사용자의 query와 유사한 RAG 결과를 생성하고, RAG 결과를 바탕으로 답변 반환 
+
+    Args:
+        state (TypedDict): Graph의 state
+    Returns:
+        Dict: LLM의 답변과 새로운 history를 반환
     """
-    messages = [{"role": "system", "content": ""}]
+    user_query = state["query"]
 
-    completion = client.chat.completions.create(model="gpt-4o-mini", messages=messages)
+    VectorDB_answer = "VectorDB 검색 결과"
 
+
+
+    messages = [
+        {"role": "system", "content": "너는 금융 도메인 전문가이자 고객 상담 AI야. VectorDB에서 제공된 정보를 근거로만 답변해야 해."},
+        {"role": "user", "content": f"다음은 VectorDB에서 찾은 정보야:\n{VectorDB_answer}"},
+        {"role": "user", "content": f"질문: {user_query}\n이 'VectorDB에서 찾은 정보'만 참고해서 사용자의 질문에 정확히 답변해줘."}
+    ]
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages,
+        max_tokens=600,
+    )
     answer = completion.choices[0].message.content
     # 히스토리에 assistant 응답 추가
-    new_history = state["history"] + [{"role": "assistant", "content": answer, "state": "new"}]
-    return state | {"answer": answer, "history": new_history}
+    new_history = {"role": "assistant", "content": answer, "state": "new"}
+    return {"answer": answer, "history": new_history}
 
 
 
