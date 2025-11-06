414faf2b34aed703ba180f7001126b0b71f2b0a4
youunaaaKim <yunao1@hanyang.ac.kr>
2025-11-06 18:32:10 +0900

    chore(db): migrate local MySQL to AWS RDS & update Django DB config

--- finbot/settings.py
+++ finbot/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 
 import environ
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,11 +85,26 @@ WSGI_APPLICATION = "finbot.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        # Django가 사용할 데이터베이스 엔진 지정
+        'ENGINE': 'django.db.backends.mysql',
+
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+
+        # 추가 옵션 설정
+        'OPTIONS': {
+            # 문자 인코딩을 utf8mb4로 설정
+            'charset': 'utf8mb4',
+            # 데이터 유효성 검사 강화: 잘못된 데이터 입력 시 경고가 아닌 오류 발생
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
 
 
 # Password validation
