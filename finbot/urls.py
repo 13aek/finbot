c23929376ac972c7b721741756602d361dbf9b23
youunaaaKim <yunao1@hanyang.ac.kr>
2025-11-08 17:43:57 +0900

    style: apply black/isort formatting

--- finbot/urls.py
+++ finbot/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("products/", include("products.urls")),
-    path("chat/", include("chatbot.urls")), 
+    path("chat/", include("chatbot.urls")),
 ]
