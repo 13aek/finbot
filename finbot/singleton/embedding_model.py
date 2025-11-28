ec5fc08d44fd78bf01638b4bcb4d3ebca6d480de
ae-tin <xodls4179@gmail.com>
2025-11-28 14:39:43 +0900

    feat: docker기반 qdrant server에 새 데이터 vector DB 저장

--- finbot/singleton/embedding_model.py
+++ finbot/singleton/embedding_model.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
 
-from FlagEmbedding import BGEM3FlagModel
+from sentence_transformers import SentenceTransformer
 
 
 @lru_cache(maxsize=1)
@@ -12,7 +12,7 @@ def get_embed_model():
     return BGEM3FlagModel : BGEM3FlagModel Embedding Model 객체
     """
     print("Singleton Embedding Model을 생성합니다....")
-    return BGEM3FlagModel("BAAI/bge-m3", use_fp16=False)
+    return SentenceTransformer("BM-K/KoSimCSE-roberta-multitask")
 
 
 embed_model = get_embed_model()
---
+++ findata/save_to_vector_db.py
@@ -0,0 +1,50 @@
+import argparse
+from pathlib import Path
+
+from findata.call_findata_api import fetch_findata
+from findata.simple_chunk import chunk
+from findata.vector_db import save_vector_db
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+save_path = BASE_DIR / "findata" / "qdrant_localdb"
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="This is saving findata to qdrant vector db")
+    # ["fixed_deposit", "installment_deposit", "jeonse_loan", "all", "all_apart"] 중 하나
+    parser.add_argument(
+        "--category",
+        "-c",
+        type=str,
+        default="fixed_deposit",
+        help="category of finance data",
+    )
+    parser.add_argument(
+        "--save_to",
+        "-s",
+        type=str,
+        default="local",
+        help="category of finance data",
+    )
+    args = parser.parse_args()
+    if args.category == "all":
+        data = []
+        data.extend(fetch_findata(category="fixed_deposit"))
+        data.extend(fetch_findata(category="installment_deposit"))
+        data.extend(fetch_findata(category="jeonse_loan"))
+
+        save_vector_db(chunk(data), category=args.category, path=save_path, save_to=args.save_to)
+
+    elif args.category == "all_apart":
+        data1 = fetch_findata(category="fixed_deposit")
+        save_vector_db(chunk(data1), category="fixed_deposit", path=save_path, save_to=args.save_to)
+
+        data2 = fetch_findata(category="installment_deposit")
+        save_vector_db(chunk(data2), category="installment_deposit", path=save_path, save_to=args.save_to)
+
+        data3 = fetch_findata(category="jeonse_loan")
+        save_vector_db(chunk(data3), category="jeonse_loan", path=save_path, save_to=args.save_to)
+
+    else:
+        data = fetch_findata(category=args.category)
+        save_vector_db(chunk(data), category=args.category, path=save_path, save_to=args.save_to)
